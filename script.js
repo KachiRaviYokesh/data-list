0d67488d5fe89aef0a929973e7fcf9c18856608d
Yokesh KR <yokesh.kr@stellaripl.com>
2025-12-12 17:06:15 +0530

    style: Icon changes & Release Year Column Added

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 const data = {
-  columns: ['ID', 'Song Name', 'Movie Name', 'Music Director', 'Movie Director', 'Artist', 'Singers', 'Lyricist', 'Language'],
+  columns: ['ID', 'Song Name', 'Movie Name', 'Movie Released Year', 'Music Director', 'Movie Director', 'Artist', 'Singers', 'Lyricist', 'Language'],
   rows: [
-    ['1001', 'Tum Hi Ho', 'Aashiqui 2', 'Mithoon', 'Mohit Suri', 'Arijit Singh', 'Arijit Singh', 'Mithoon', 'Hindi'],
-    ['1002', 'Chaiyya Chaiyya', 'Dil Se..', 'A. R. Rahman', 'Mani Ratnam', 'Shah Rukh Khan', 'Sukhwinder Singh, Sapna Awasthi', 'Gulzar', 'Hindi'],
-    ['1003', 'Kal Ho Naa Ho', 'Kal Ho Naa Ho', 'Shankar-Ehsaan-Loy', 'Nikkhil Advani', 'Sonu Nigam', 'Sonu Nigam', 'Javed Akhtar', 'Hindi'],
-    ['1004', 'Bulleya', 'Ae Dil Hai Mushkil', 'Pritam', 'Karan Johar', 'Amit Mishra, Shilpa Rao', 'Amit Mishra, Shilpa Rao', 'Amitabh Bhattacharya', 'Hindi'],
-    ['1005', 'Ghoomar', 'Padmaavat', 'Sanjay Leela Bhansali', 'Sanjay Leela Bhansali', 'Shreya Ghoshal, Swaroop Khan', 'Shreya Ghoshal, Swaroop Khan', 'A. M. Turaz', 'Hindi'],
-    ['1006', 'Jai Ho', 'Slumdog Millionaire', 'A. R. Rahman', 'Danny Boyle', 'A. R. Rahman, Sukhwinder Singh', 'A. R. Rahman, Sukhwinder Singh', 'Gulzar', 'Hindi'],
-    ['1007', 'Tera Ban Jaunga', 'Kabir Singh', 'Akhil Sachdeva, Tulsi Kumar', 'Sandeep Reddy Vanga', 'Akhil Sachdeva, Tulsi Kumar', 'Kumaar', 'Hindi'],
-    ['1008', 'Apna Time Aayega', 'Gully Boy', 'Dub Sharma, Divine', 'Zoya Akhtar', 'Ranveer Singh, Divine', 'Divine, Ankur Tewari', 'Hindi'],
-    ['1009', 'Bekhayali', 'Kabir Singh', 'Sachet-Parampara', 'Sandeep Reddy Vanga', 'Sachet Tandon', 'Irshad Kamil', 'Hindi'],
-    ['1010', 'Ghar More Pardesiya', 'Kalank', 'Pritam', 'Abhishek Varman', 'Shreya Ghoshal, Vaishali Mhade', 'Amitabh Bhattacharya', 'Hindi'],
-    ['1011', 'Ghar More Pardesiya', 'Kalank', 'Pritam', 'Abhishek Varman', 'Shreya Ghoshal, Vaishali Mhade', 'Amitabh Bhattacharya', 'Hindi'],
-    ['1012', 'Ghar More Pardesiya', 'Kalank', 'Pritam', 'Abhishek Varman', 'Shreya Ghoshal, Vaishali Mhade', 'Amitabh Bhattacharya', 'Hindi'],
-    ['1013', 'Ghar More Pardesiya', 'Kalank', 'Pritam', 'Abhishek Varman', 'Shreya Ghoshal, Vaishali Mhade', 'Amitabh Bhattacharya', 'Hindi'],
-    ['1014', 'Ghar More Pardesiya', 'Kalank', 'Pritam', 'Abhishek Varman', 'Shreya Ghoshal, Vaishali Mhade', 'Amitabh Bhattacharya', 'Hindi'],
-    ['1015', 'Ghar More Pardesiya', 'Kalank', 'Pritam', 'Abhishek Varman', 'Shreya Ghoshal, Vaishali Mhade', 'Amitabh Bhattacharya', 'Hindi'],
+    ['1001', 'Tum Hi Ho', 'Aashiqui 2', '2013', 'Mithoon', 'Mohit Suri', 'Arijit Singh', 'Arijit Singh', 'Mithoon', 'Hindi'],
+    ['1002', 'Chaiyya Chaiyya', 'Dil Se..', '1998', 'A. R. Rahman', 'Mani Ratnam', 'Shah Rukh Khan', 'Sukhwinder Singh, Sapna Awasthi', 'Gulzar', 'Hindi'],
+    ['1003', 'Kal Ho Naa Ho', 'Kal Ho Naa Ho', '2003', 'Shankar-Ehsaan-Loy', 'Nikkhil Advani', 'Sonu Nigam', 'Sonu Nigam', 'Javed Akhtar', 'Hindi'],
+    ['1004', 'Bulleya', 'Ae Dil Hai Mushkil', '2016', 'Pritam', 'Karan Johar', 'Amit Mishra, Shilpa Rao', 'Amit Mishra, Shilpa Rao', 'Amitabh Bhattacharya', 'Hindi'],
+    ['1005', 'Ghoomar', 'Padmaavat', '2018', 'Sanjay Leela Bhansali', 'Sanjay Leela Bhansali', 'Shreya Ghoshal, Swaroop Khan', 'Shreya Ghoshal, Swaroop Khan', 'A. M. Turaz', 'Hindi'],
+    ['1006', 'Jai Ho', 'Slumdog Millionaire', '2008', 'A. R. Rahman', 'Danny Boyle', 'A. R. Rahman, Sukhwinder Singh', 'A. R. Rahman, Sukhwinder Singh', 'Gulzar', 'Hindi'],
+    ['1007', 'Tera Ban Jaunga', 'Kabir Singh', '2019', 'Akhil Sachdeva, Tulsi Kumar', 'Sandeep Reddy Vanga', 'Akhil Sachdeva, Tulsi Kumar', 'Kumaar', 'Hindi'],
+    ['1008', 'Apna Time Aayega', 'Gully Boy', '2019', 'Dub Sharma, Divine', 'Zoya Akhtar', 'Ranveer Singh, Divine', 'Divine, Ankur Tewari', 'Hindi'],
+    ['1009', 'Bekhayali', 'Kabir Singh', '2019', 'Sachet-Parampara', 'Sandeep Reddy Vanga', 'Sachet Tandon', 'Irshad Kamil', 'Hindi'],
+    ['1010', 'Ghar More Pardesiya', 'Kalank', '2019', 'Pritam', 'Abhishek Varman', 'Shreya Ghoshal, Vaishali Mhade', 'Amitabh Bhattacharya', 'Hindi'],
+    ['1011', 'Ghar More Pardesiya', 'Kalank', '2019', 'Pritam', 'Abhishek Varman', 'Shreya Ghoshal, Vaishali Mhade', 'Amitabh Bhattacharya', 'Hindi'],
+    ['1012', 'Ghar More Pardesiya', 'Kalank', '2019', 'Pritam', 'Abhishek Varman', 'Shreya Ghoshal, Vaishali Mhade', 'Amitabh Bhattacharya', 'Hindi'],
+    ['1013', 'Ghar More Pardesiya', 'Kalank', '2019', 'Pritam', 'Abhishek Varman', 'Shreya Ghoshal, Vaishali Mhade', 'Amitabh Bhattacharya', 'Hindi'],
+    ['1014', 'Ghar More Pardesiya', 'Kalank', '2019', 'Pritam', 'Abhishek Varman', 'Shreya Ghoshal, Vaishali Mhade', 'Amitabh Bhattacharya', 'Hindi'],
+    ['1015', 'Ghar More Pardesiya', 'Kalank', '2019', 'Pritam', 'Abhishek Varman', 'Shreya Ghoshal, Vaishali Mhade', 'Amitabh Bhattacharya', 'Hindi'],
   ]
 };
 
@@ -29,37 +29,10 @@ const state = {
 };
 
 const icons = {
-  sun: `
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-      <circle cx="12" cy="12" r="5"></circle>
-      <line x1="12" y1="1" x2="12" y2="3"></line>
-      <line x1="12" y1="21" x2="12" y2="23"></line>
-      <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
-      <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
-      <line x1="1" y1="12" x2="3" y2="12"></line>
-      <line x1="21" y1="12" x2="23" y2="12"></line>
-      <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
-      <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
-    </svg>
-  `,
-  moon: `
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M21.66 12.66C20.01 16.92 15.81 20 11 20C6.19 20 2 16.92 0.34 12.66C-1.31 8.4 2.89 5.34 7.71 5.34C12.52 5.34 16.72 8.4 18.38 12.66C19.05 14.33 19.38 16.11 19.38 17.91C19.38 18.57 19.34 19.22 19.27 19.86C20.6 18.53 21.4 16.67 21.66 14.66C21.74 14.01 21.78 13.34 21.78 12.66C21.78 12.66 21.78 12.66 21.66 12.66Z" fill="currentColor"/>
-    </svg>
-  `,
-  edit: `
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-      <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"/>
-    </svg>
-  `,
-  delete: `
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-      <polyline points="3 6 5 6 21 6"></polyline>
-      <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-      <line x1="10" y1="11" x2="10" y2="17"></line>
-      <line x1="14" y1="11" x2="14" y2="17"></line>
-    </svg>
-  `
+  sun: `<img src="https://openmoji.org/data/color/svg/2600.svg" alt="sun" width="24" height="24">`,
+  moon: `<img src="https://openmoji.org/data/color/svg/1F315.svg" alt="moon" width="24" height="24">`,
+  edit: `<img src="https://openmoji.org/data/color/svg/1F58B.svg" alt="edit" width="24" height="24">`,
+  delete: `<img src="https://openmoji.org/data/color/svg/1F5D1.svg" alt="delete" width="24" height="24">`
 };
 
 const els = {
